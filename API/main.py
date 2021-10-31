83436750bc9634781e6900a9803f087823f6f6e8
Ryan Radloff <ryan.radloff01@gmail.com>
2021-10-31 08:42:01 -0500

    Initial implementation of the addWord API

--- API/main.py
+++ API/main.py
@@ -1,24 +1,51 @@
 # app.py
 
 # Required imports
-import os
-from flask import Flask, request, jsonify
+import time
+import joblib
+import sys
+
+from flask import Flask, request
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize import RegexpTokenizer
 
 # Initialize Flask app
 app = Flask(__name__)
 Sentences= {}
 
+Emotions = ["Neutral", "Anger", "Disgust", "Fear", "Happiness", "Sadness", "Surprise"]
+TextModel = joblib.load('./data/textclassifier/classifier.joblib.pkl')
+TextModelVectorizer = joblib.load('./data/textclassifier/vectorizer.joblib.pkl')
+
 @app.route('/addWord', methods=['POST'])
 def create():
     # Ryan code here
-    str(request.get_data)
-    now = int(time.time())
-    Sentences[now] = ""
-    return now
+    returnBody = ""
+    data = str(request.get_data(as_text=True))
+    sentence = data.split(",")
+
+    if str(sentence[0]) not in Sentences:
+        now = int(time.time())
+        Sentences[str(now)] = ""
+        return str(now)
+    
+    
+    Sentences[sentence[0]] = Sentences[sentence[0]] + sentence[1]
+    matrix = TextModelVectorizer.transform([Sentences[sentence[0]]])
+    predictions = TextModel.predict_proba(matrix)
+    predictions = predictions[0].tolist()
+    for i in range(0, len(predictions)):
+        returnBody = returnBody + Emotions[i] + ":"
+        returnBody = returnBody + str(predictions[i]) + ","
+    
+    return returnBody[:-1]
+
+
 
 @app.route('/endSentence', methods=['GET'])
 def read():
     # Johnathan code here
+    print("hey")
 
 
-app.run(threaded=True, host='0.0.0.0', port=8080)
+app.run(threaded=True, host='localhost', port=8081)
