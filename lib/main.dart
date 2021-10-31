5e31be24e092de67fc28635318f39dd447925fcc
Jacob Dong <jjd180002@utdallas.edu>
2021-10-31 00:27:03 -0500

    ok first app iteration, simple

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:speech_to_text_example/page/home_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Tone Tendency';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+          appBarTheme: AppBarTheme(
+            color: const Color(0xFF151026),
+          )
+        ),
+        home: HomePage(),
+      );
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:clipboard/clipboard.dart';
+import 'package:flutter/material.dart';
+import 'package:speech_to_text_example/api/speech_api.dart';
+import 'package:speech_to_text_example/main.dart';
+import 'package:speech_to_text_example/widget/substring_highlighted.dart';
+
+import '../utils.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String text = 'Press the button and start speaking';
+  bool isListening = false;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          reverse: true,
+          padding: const EdgeInsets.all(30).copyWith(bottom: 150),
+          child: SubstringHighlight(
+            text: text,
+            terms: Command.all,
+            textStyle: TextStyle(
+              fontSize: 32.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+            textStyleHighlight: TextStyle(
+              fontSize: 32.0,
+              color: Colors.red,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+            child: Icon(isListening ? Icons.mic : Icons.mic_none, size: 36),
+            onPressed: () {
+              toggleRecording();
+            },
+          backgroundColor: Colors.black,
+          ),
+      );
+
+  Future toggleRecording() => SpeechApi.toggleRecording(
+        onResult: (text) => setState(() => this.text = text),
+        onListening: (isListening) {
+          setState(() => this.isListening = isListening);
+
+          if (!isListening) {
+            Future.delayed(Duration(seconds: 1), () {
+              Utils.scanText(text);
+            });
+          }
+        },
+      );
+}
